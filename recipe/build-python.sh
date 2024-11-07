8bbd9ba56d396382792fca1835c3c238edbe6ca6
anaobi <anaobii@gmail.com>
2024-11-07 11:17:15 +0000

    first build

--- recipe/build-python.sh
+++ recipe/build-python.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+
+# Run CMake with specified configurations
 cmake -G Ninja \
       ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
@@ -10,7 +12,7 @@ cmake -G Ninja \
       -DBUILD_SHARED_LIBS=ON \
       -DBUILD_DEPS=OFF \
       -DBUILD_Eigen3=ON \
-      -DBUILD_absl=ON \
+      -DBUILD_absl=OFF \
       -DUSE_SCIP=OFF \
       -S. \
       -Bbuild \
@@ -26,10 +28,13 @@ cmake -G Ninja \
       -DPython3_FIND_VIRTUALENV=ONLY \
       -DPython3_FIND_STRATEGY=LOCATION \
       -DPython3_EXECUTABLE=${PREFIX}/bin/python \
-      -DPython3_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,2%.lib
+      -DPython3_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,2%.lib 
 
+# Build with the specified number of processors
 cmake --build build -j"${CPU_COUNT}"
 
 echo Install begins here
 
+# Install Python package
 ${PYTHON} -m pip install --no-index --find-links=build/python/dist ortools -vv
+
