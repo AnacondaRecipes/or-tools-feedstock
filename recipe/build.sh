852a11ec3e80143c6e8fdc19afc6c6a8ac74f8ea
anaobi <anaobii@gmail.com>
2024-11-11 12:44:20 +0000

    building from .whl

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ortools-9.11.4210-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+#$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ortools-9.11.4210-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed -vv .
 
