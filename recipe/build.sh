08534bd7fb48c790f2817dc09d384d677767bcd4
anaobi <anaobii@gmail.com>
2024-12-02 09:37:52 +0100

    add windows .whl

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 $PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ortools-9.11.4210-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-#$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed -vv "${SRC_DIR}/."
+
+$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ortools-9.11.4210-cp311-cp311-win_amd64.whl"
+
 
